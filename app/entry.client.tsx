9fce922f02125c0dc7d54a54f4576c084a1a691d
M Haidar Hanif <git@mhaidarhanif.com>
2025-02-07 23:27:37 +0700

    chore: default template and upgrade deps

--- app/entry.client.tsx
+++
@@ -1,7 +0,0 @@
-import { startTransition, StrictMode } from "react";
-import { hydrateRoot } from "react-dom/client";
-import { HydratedRouter } from "react-router/dom";
-
-startTransition(() => {
-  hydrateRoot(document, <HydratedRouter />);
-});
