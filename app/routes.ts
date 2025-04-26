6b9ba7e6806b63d642643ec591fd0555d3df7e35
M Haidar Hanif <git@mhaidarhanif.com>
2025-04-27 03:37:09 +0700

    fix: try to debug setup/organization route

--- app/routes.ts
+++ app/routes.ts
@@ -9,22 +9,22 @@ export default [
   layout("layouts/main.tsx", [
     index("routes/index.tsx"),
 
-    route("/jobs", "routes/jobs/list.tsx"),
-    route("/jobs/:slug", "routes/jobs/slug.tsx"),
+    route("jobs", "routes/jobs/list.tsx"),
+    route("jobs/:slug", "routes/jobs/slug.tsx"),
 
-    route("/jobs/saved", "routes/jobs/saved.tsx"),
-    route("/jobs/applied", "routes/jobs/applied.tsx"),
-    route("/jobs/progress", "routes/jobs/progress.tsx"),
-    route("/jobs/canceled", "routes/jobs/canceled.tsx"),
+    route("jobs/saved", "routes/jobs/saved.tsx"),
+    route("jobs/applied", "routes/jobs/applied.tsx"),
+    route("jobs/progress", "routes/jobs/progress.tsx"),
+    route("jobs/canceled", "routes/jobs/canceled.tsx"),
 
-    route("/organizations", "routes/organizations/list.tsx"),
-    route("/organizations/:slug", "routes/organizations/slug.tsx"),
+    route("organizations", "routes/organizations/list.tsx"),
+    route("organizations/:slug", "routes/organizations/slug.tsx"),
 
-    route("/search", "routes/search/result.tsx"),
+    route("search", "routes/search/result.tsx"),
 
-    route("/post-a-job", "routes/post-a-job.tsx"),
+    route("post-a-job", "routes/post-a-job.tsx"),
 
-    route("/setup/organization", "routes/setup/organization.tsx"),
+    route("setup/organization", "routes/setup/organization.tsx"),
   ]),
 
   route("editor", "routes/editor/demo.tsx"),
